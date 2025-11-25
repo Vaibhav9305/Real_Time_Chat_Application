988281996269898b648bc9ffbeb4d9639d3023e8
Vaibhav9305 <vaibhav27092003@gmail.com>
2025-11-26 01:46:44 +0530

    Changes for connection

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,33 @@
 // const express = require('express');  //type-module
-import express from 'express';        //ES module
-import dotenv from 'dotenv';
-import connectDB from './config/database.js';
-import userRoutes from './routes/userRoute.js';
-import messageRoute from './routes/messageRoute.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { server, app} from './socket/socket.js';
+import express from "express"; //ES module
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoutes from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { server, app } from "./socket/socket.js";
 
 dotenv.config({});
-
 
 const PORT = process.env.PORT || 3000;
 
 //middleware
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: process.env.CLIENT_URL,
-  credentials: true
+  origin: [
+    "http://localhost:5173", // for local dev
+    "https://real-time-chat-git-c36e1c-vaibhav-srivastavas-projects-22b78c5b.vercel.app", // deployed frontend
+  ],
+  credentials: true,
 };
 app.use(cors(corsOption));
 
 //routes
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/message", messageRoute)
+app.use("/api/v1/message", messageRoute);
 
 server.listen(PORT, () => {
   connectDB();
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
-import http from 'http';
-import express from 'express';
-import dotenv, { config } from 'dotenv';
+import http from "http";
+import express from "express";
+import dotenv, { config } from "dotenv";
 
 dotenv.config({});
 
@@ -9,33 +9,36 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:[process.env.CLIENT_URL],
-        methods:['GET', 'POST'],
-    },
+  cors: {
+    origin: [
+      "http://localhost:5173", // for local dev
+      "https://real-time-chat-git-c36e1c-vaibhav-sprojects-22b78c5b.vercel.apprivastavas-", // deployed frontend
+    ],
+    methods: ["GET", "POST"],
+  },
 });
 
-export const getReceiverSocketId = (receiverId)=>{
-    return userSocketMap[receiverId];
-}
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
 
 const userSocketMap = {};
 
-io.on('connection', (socket)=>{
-    console.log('User connected', socket.id);
-    
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
-       userSocketMap[userId]= socket.id;
-    }
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
 
-    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+  const userId = socket.handshake.query.userId;
+  if (userId !== undefined) {
+    userSocketMap[userId] = socket.id;
+  }
 
-    socket.on('disconnect', ()=>{
-        console.log('User disconnected', socket.id);
-        delete userSocketMap[userId];
-        io.emit('getOnlineUsers', Object.keys(userSocketMap));
-    })
-})
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-export {app, io, server};
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, io, server };
