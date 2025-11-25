3a73676e742ac5aa2337bd496536e43f961436e1
Vaibhav9305 <vaibhav27092003@gmail.com>
2025-11-26 00:48:04 +0530

    Hiding Essentials

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+    await mongoose.connect(mongoURI).then(() => {
         console.log("MongoDB connected successfully");
     }).catch((err) => {
         console.log("MongoDB connection failed:", err);
