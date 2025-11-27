52f01cc8e6b19707e2850eb165feda2d81396d7f
Vaibhav Srivastava <119913481+Vaibhav9305@users.noreply.github.com>
2025-11-28 03:15:50 +0530

    Link updation

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://real-time-chat-git-c36e1c-vaibhav-srivastavas-projects-22b78c5b.vercel.app",
+      "https://chat-mern-khaki.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST"],
